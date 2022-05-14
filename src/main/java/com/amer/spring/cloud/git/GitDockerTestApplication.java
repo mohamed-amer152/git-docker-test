34018a240f8464a8bacebcb2d08dd4a6129df050
mohamed-amer152 <rezk2562@gmail.com>
2022-05-14 20:04:38 +0400

    updated the rest controller

--- src/main/java/com/amer/spring/cloud/git/GitDockerTestApplication.java
+++ src/main/java/com/amer/spring/cloud/git/GitDockerTestApplication.java
@@ -2,9 +2,18 @@ package com.amer.spring.cloud.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitDockerTestApplication {
+	
+	@GetMapping("/")
+	public String message()
+	{
+		return " hello prince " ;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDockerTestApplication.class, args);
